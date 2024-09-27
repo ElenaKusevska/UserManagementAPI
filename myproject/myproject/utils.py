8f909500707809385bca0204b5102ee706906473
ElenaKusevska <elena.kusevska@gmail.com>
2024-09-27 17:18:32 +0200

    check request data is valid example

---
+++ myproject/myproject/utils.py
@@ -0,0 +1,6 @@
+from rest_framework.exceptions import APIException
+
+class PasswordTooShortException(APIException):
+    status_code = 503
+    default_detail = 'Password is too short, must contain at least 8 characters'
+    default_code = 'Invalid Password'
--- myproject/users/views.py
+++ myproject/users/views.py
@@ -2,6 +2,8 @@ from .models import User
 from .serializers import UserSerializer
 from rest_framework import viewsets
 from rest_framework.response import Response
+from myproject.utils import PasswordTooShortException
+
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -9,6 +11,25 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+    def validate_request_data(self, request_method, request_data):
+
+        # For example to check that password is not too short:
+        if request_method == "POST":
+            if request_data["password"] and len(request_data["password"]) < 8:
+                raise(PasswordTooShortException)
+
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        self.validate_request_data(context["request"].method, context["request"].data,)
+        return context
+
+        
+
+
+
     
 
 
