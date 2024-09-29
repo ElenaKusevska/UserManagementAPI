b7ce01bbc821bc60314ec747a2236da58d052131
ElenaKusevska <elena.kusevska@gmail.com>
2024-09-29 12:31:40 +0200

    add appropriate status code for PasswordTooShortException

--- myproject/myproject/utils.py
+++ myproject/myproject/utils.py
@@ -1,6 +1,6 @@
 from rest_framework.exceptions import APIException
 
 class PasswordTooShortException(APIException):
-    status_code = 503
+    status_code = 422
     default_detail = 'Password is too short, must contain at least 8 characters'
     default_code = 'Invalid Password'
