513acc9a660783a61a86f13661a2618487c82684
ElenaKusevska <elena.kusevska@gmail.com>
2024-09-29 12:49:23 +0200

    test that api returns appropriate error when post request to create user with password too short

--- myproject/users/tests.py
+++ myproject/users/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from .models import User
+from myproject.utils import PasswordTooShortException
+
 
 from rest_framework.test import RequestsClient
 
@@ -9,7 +11,7 @@ class UserTestCase(TestCase):
         User.objects.create(username="user1", email="u1@g.com")
         User.objects.create(username="user2", email="u2@g.com", age="22")
 
-    def test_post_users(self):
+    def test_post_user(self):
         users = User.objects.all()
         assert len(users) == 2
 
@@ -20,5 +22,12 @@ class UserTestCase(TestCase):
 
         users = User.objects.all()
         assert len(users) == 3
+
+    def test_post_user_password_too_short(self):
+        client = RequestsClient()
+        response = client.post('http://testserver/users/', {'username': 'user3', 'email': 'e3@g.com', 'password': 'p1p'})
+
+        assert response.status_code == 422
+        assert "Password is too short" in str(response.content)
         
 
